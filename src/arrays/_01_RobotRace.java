8aa0ae6f4e5b4d9c840839592c9b205542270e62
1862386 <roshanjshah@gmail.com>
2020-06-11 19:57:04 -0700

    Robot race

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -15,12 +15,18 @@ public class _01_RobotRace {
 		for (int i = 0; i < robots.length; i++) {
 			Robot bot = new Robot ();
 		robots[i] = bot;
+		robots[i].setX(200);
+		robots[i].setY(550);
 		}
 		// 4. make each robot start at the bottom of the screen, side by side, facing up
-		robots.
+		
 		// 5. use another for loop to iterate through the array and make each robot move
 		// a random amount less than 50.
-
+		//for (int i = 0; i < robots.length; i++) {
+		//	int rand = (int) (Math.random() * 50);
+		//	name[i] = rand;
+			//System.out.println(rand);
+		}
 		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
 		// screen.
   
@@ -28,6 +34,6 @@ public class _01_RobotRace {
 
 		// 8. try different races with different amounts of robots.
 
-		// 9. make the robots race around a circular track.
+		// 9. make the robots race around a circular track.``````
 	}
 }
